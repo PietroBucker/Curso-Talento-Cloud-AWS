1e445f0ebeca04636ed23fb7a4354bf080c59c37
Pietro Bucker <pietro_177@hotmail.com>
2023-07-28 15:28:28 -0300

    aula 3 ex1 calculadora fix

--- introducao-programaçao/aula3/exercicio1.py
+++ introducao-programaçao/aula3/exercicio1.py
@@ -6,7 +6,9 @@ def calculator(n1, n2, op):
     if op == "*":
         return n1 * n2
     if op == "/":
-        return f'{n1 / n2:.2f}'
+        return f"{n1 / n2:.2f}"
+    else:
+        return 0
 
 
-print(calculator(2, 3, "/"))
+print(calculator(2, 3, "//"))
