bf5342ba3a4777e8c01b317dfb87b6044b1a470c
Pietro Bucker <pietro_177@hotmail.com>
2023-09-27 20:50:40 -0300

    exercicio 4

--- JS/exercicio4.js
+++ JS/exercicio4.js
@@ -5,7 +5,7 @@ function verifica(array) {
         if (element % 2 == 0 && element < 50) {
             console.log('X é par e menor que 50');
         }
-        if (element < 50) {
+        else if (element < 50) {
             console.log('X é menor que 50');
         }
         else {
