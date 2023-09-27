694687383d59d176cdf8956dc4970bc195e244cf
Pietro Bucker <pietro_177@hotmail.com>
2023-09-27 20:31:33 -0300

    exercicio templateString

---
+++ JS/templateString.js
@@ -0,0 +1,8 @@
+function calculadora(a, b) {
+    console.log(`${a} + ${b} = ${a + b}`);
+    console.log(`${a} - ${b} = ${a - b}`);
+    console.log(`${a} X ${b} = ${a * b}`);
+    console.log(`${a} / ${b} = ${a / b}`);
+}
+
+calculadora(4, 5)
